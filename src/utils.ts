5fa2b496601367c7b540d2f85fcb3ab90de93715
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-12-14 03:43:11 +0000

    fix: didn't mean to remove that

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,14 @@
 import { keccak256 } from "@ethersproject/solidity"
+import { ZqField } from 'ffjavascript'
+
+/*
+  This is the "Baby Jubjub" curve described here:
+  https://iden3-docs.readthedocs.io/en/latest/_downloads/33717d75ab84e11313cc0d8a090b636f/Baby-Jubjub.pdf
+*/
+export const SNARK_FIELD_SIZE = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
+
+// Creates the finite field
+export const Fq = new ZqField(SNARK_FIELD_SIZE)
 
 /**
  * Generates an External Nullifier for use with RLN.
