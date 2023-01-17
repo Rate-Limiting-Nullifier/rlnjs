1f164a7f96f23496068c16b332dd1ea0d1346e91
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-01-17 23:42:30 +0000

    chore: build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
 import RLN from "./rln";
 import Registry from './registry';
 import Cache from './cache';
-export { RLN, Registry, Cache };
+import { genExternalNullifier } from "./utils";
+export { RLN, Registry, Cache, genExternalNullifier };
 export { StrBigInt, RLNFullProof, Proof, RLNPublicSignals } from "./types/rlnjs";
--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,5 +1,6 @@
+import { ZqField } from 'ffjavascript';
 export declare const SNARK_FIELD_SIZE: bigint;
-export declare const Fq: any;
+export declare const Fq: ZqField;
 /**
  * Generates an External Nullifier for use with RLN.
  * @param plaintext String. //TODO: better description
