45f4ddb1e7b24b28b3674dd0a2cb29c002b89a0e
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-02-06 17:42:58 +0000

    adding dist for external testing

---
+++ dist/cache.d.ts
@@ -0,0 +1,47 @@
+import { RLNFullProof, StrBigInt } from './types';
+type EpochCacheT = {
+    nullifiers?: RLNFullProof[];
+};
+export declare enum Status {
+    ADDED = "added",
+    BREACH = "breach",
+    INVALID = "invalid"
+}
+export type EvaluatedProof = {
+    status: Status;
+    nullifier?: StrBigInt;
+    secret?: StrBigInt;
+    msg?: string;
+};
+/**
+ * Cache for storing proofs and automatically evaluating them for rate limit breaches
+ */
+export default class Cache {
+    cache: {
+        string?: EpochCacheT;
+    };
+    epochs: string[];
+    cacheLength: number;
+    rln_identifier: StrBigInt;
+    /**
+     *
+     * @param cacheLength the maximum number of epochs to store in the cache, default is 100, set to 0 to automatic pruning
+     */
+    constructor(rln_identifier: StrBigInt, cacheLength?: number);
+    get _cache(): {
+        string?: EpochCacheT | undefined;
+    };
+    get _epochs(): string[];
+    /**
+     *  Adds a proof to the cache
+     * @param proof the RLNFullProof to add to the cache
+     * @returns an object with the status of the proof and the nullifier and secret if the proof is a breach
+     */
+    addProof(proof: RLNFullProof): EvaluatedProof;
+    private evaluateNullifierAtEpoch;
+    private evaluateEpoch;
+    private removeEpoch;
+    export(): Promise<string>;
+    static import(cache: string): Promise<Cache>;
+}
+export {};
---
+++ dist/types.d.ts
@@ -0,0 +1,20 @@
+export type StrBigInt = string | bigint;
+export type Proof = {
+    pi_a: StrBigInt[];
+    pi_b: StrBigInt[][];
+    pi_c: StrBigInt[];
+    protocol: string;
+    curve: string;
+};
+export type RLNFullProof = {
+    proof: Proof;
+    publicSignals: RLNPublicSignals;
+};
+export type RLNPublicSignals = {
+    yShare: StrBigInt;
+    merkleRoot: StrBigInt;
+    internalNullifier: StrBigInt;
+    signalHash: StrBigInt;
+    epoch: StrBigInt;
+    rlnIdentifier: StrBigInt;
+};
