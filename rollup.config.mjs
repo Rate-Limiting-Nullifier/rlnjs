d20f802f9b1297d46b6a8f46eac378b73a96e8db
mhchia <kevin.mh.chia@gmail.com>
2023-03-15 21:29:43 +0800

    chore: add plugins in rollup to fix bundled js
    - fix "process is not defined" with `replace` plugin
    - fix "window undefined" with `nodeResolve` plugin with `{browser: true}`
    - use `json` plugin to make parse and import json from es module
    - use `nodePolyfills` to replace node built-ins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,15 @@
-
 import typescript from 'rollup-plugin-typescript2';
-import cleaner from 'rollup-plugin-cleaner';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+import replace from '@rollup/plugin-replace';
 import { visualizer } from "rollup-plugin-visualizer";
+import cleaner from 'rollup-plugin-cleaner';
 import * as fs from "fs"
 
+
+const input = 'src/index.ts'
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf-8"))
 const banner = `/**
  * @module ${pkg.name}
@@ -15,27 +21,72 @@ const banner = `/**
 */`
 
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    { file: pkg.exports.require, format: "cjs", banner, exports: "auto" },
-    { file: pkg.exports.import, format: "es", banner }
-  ],
-  external: Object.keys(pkg.dependencies),
-  plugins: [
-    cleaner({
-      targets: [
-        './dist/'
-      ]
-    }),
-    typescript({
-      tsconfig: 'tsconfig.build.json',
-      useTsconfigDeclarationDir: true
-    }),
-    visualizer({
-      emitFile: true,
-      filename: "stats.html",
-      template: "sunburst"
-    }),
-  ]
-};
+const typescriptPlugin = typescript({
+  tsconfig: 'tsconfig.build.json',
+  useTsconfigDeclarationDir: true
+})
+
+const visualizerPlugin = visualizer({
+  emitFile: true,
+  filename: "stats.html",
+  template: "sunburst"
+})
+
+const nodePlugins = [
+  typescriptPlugin,
+  // `browser: false` is required for `fs` and other Node.js core modules to be resolved correctly
+  nodeResolve({ browser: false }),
+  // To accept commonjs modules and convert them to ES module, since rollup only bundle ES modules by default
+  commonjs(),
+  // Parse JSON files and make them ES modules. Required when bundling circomlib
+  json(),
+];
+
+const browserPlugins = [
+  typescriptPlugin,
+  replace({
+    // Replace `process.browser` with `true` to avoid `process is not defined` error
+    // This is because ffjavascript and snarkjs use `process.browser` to check if it's running in the browser,
+    // but process is undefined in the browser and referencing `process.browser` causes an error.
+    // Ref: https://github.com/iden3/ffjavascript/blob/e670bfeb17e80b961eab77e15a6b9eca8e31a0be/src/threadman.js#L43
+    'process.browser': JSON.stringify(true),
+    // To avoid unexpected behavior that the warning suggests.
+    'preventAssignment': true,
+  }),
+  // Resolve the import from node_modules.
+  // `browser: true` is required for `window` not to be undefined
+  // Ref: https://github.com/iden3/snarkjs/blob/782894ab72b09cfad4dd8b517599d5e7b2340468/src/taskmanager.js#L20-L24
+  nodeResolve({browser: true}),
+  commonjs(),
+  json(),
+  // Replace node built-in modules with polyfills
+  nodePolyfills(),
+];
+
+
+export default [
+  // Node.js build
+  {
+    input,
+    output: { file: pkg.exports.require, format: "cjs", banner },
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+      cleaner({
+        targets: [
+          './dist/'
+        ]
+      }),
+      ...nodePlugins,
+      visualizerPlugin,
+    ]
+  },
+  // Browser build
+  {
+    input,
+    output: { file: pkg.exports.import, format: "es", banner },
+    plugins: [
+      ...browserPlugins,
+      visualizerPlugin,
+    ]
+  }
+]
