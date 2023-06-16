c024ff5207655733aee7d8393faa2538be190cf0
mhchia <kevin.mh.chia@gmail.com>
2023-06-17 00:34:43 +0800

    remove dist

--- dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import RLN from './rln';
-import Registry from './registry';
-import Cache from './cache';
-export { RLN, Registry, Cache, };
-export { StrBigInt, RLNFullProof, Proof, RLNPublicSignals, RLNSNARKProof, VerificationKey, RLNWitness, CircuitParamsFilePath, } from './types';
