0fbd500cdcc0520781b9f9044e5000f32c3ef9fc
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-06-25 06:14:09 +0000

    Updating type exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 // Imports
 export { IRLN, RLN } from './rln'
-export { IRLNRegistry, ContractRLNRegistry }  from './registry'
+export { IRLNRegistry, ContractRLNRegistry } from './registry'
 export { ICache, MemoryCache, CachedProof, Status } from './cache'
 export { IMessageIDCounter } from './message-id-counter'
 
 export * from './types'
 
-export {
-  RLNFullProof,
-} from './circuit-wrapper'
+export { RLNFullProof, RLNSNARKProof, RLNWitness, RLNPublicSignals } from './circuit-wrapper'
