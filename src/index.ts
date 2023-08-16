1db1949695402fc1d0c7d4d01b181be67ce95222
mhchia <kevin.mh.chia@gmail.com>
2023-08-16 17:21:59 +0800

    feat: expose more helper functions

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ export { CachedProof, ICache, MemoryCache, Status } from './cache'
 export { IMessageIDCounter } from './message-id-counter'
 
 export * from './types'
+// Expose helpers
+export { calculateExternalNullifier,  calculateRateCommitment, calculateSignalHash, shamirRecovery } from "./common"
 
 export { RLNFullProof, RLNSNARKProof, RLNWitness, RLNPublicSignals, RLNProver, RLNVerifier, WithdrawProver } from './circuit-wrapper'
