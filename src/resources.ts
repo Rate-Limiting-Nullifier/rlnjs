e013ac262ee2540116e5afa198ff42c5cd7d68d0
nt <42067723+phi7@users.noreply.github.com>
2023-09-23 09:13:02 +0900

    change to permanent urls

--- src/resources.ts
+++ src/resources.ts
@@ -3,17 +3,17 @@ import { VerificationKey } from './types'
 import axios from 'axios'
 
 type ICircuitParams = {
-  wasmFile: string | Uint8Array,
-  finalZkey: string | Uint8Array,
+  wasmFile: string | Uint8Array;
+  finalZkey: string | Uint8Array;
 }
 
 type IRLNParams = ICircuitParams & { verificationKey: VerificationKey }
 type IWithdrawParams = ICircuitParams
 
-// TODO: Change to a more permanent URL after trusted setup is complete
-const resourcesURL = 'https://rln-resources-temp.s3.us-west-1.amazonaws.com/resources'
+const resourcesURL =
+  'https://rln-trusted-setup-ceremony-pse-p0tion-production.s3.eu-central-1.amazonaws.com/circuits'
 const rln20URL = `${resourcesURL}/rln-20`
-const withdrawURL = `${resourcesURL}/withdraw`
+const withdrawURL = `${resourcesURL}/rln-withdraw`
 const treeDepthToDefaultRLNParamsURL = {
   '20': rln20URL,
 }
@@ -32,7 +32,8 @@ function parseVerificationKeyJSON(o: any): VerificationKey {
   if (!o.vk_beta_2) throw new Error('Verification key has no vk_beta_2')
   if (!o.vk_gamma_2) throw new Error('Verification key has no vk_gamma_2')
   if (!o.vk_delta_2) throw new Error('Verification key has no vk_delta_2')
-  if (!o.vk_alphabeta_12) throw new Error('Verification key has no vk_alphabeta_12')
+  if (!o.vk_alphabeta_12)
+    throw new Error('Verification key has no vk_alphabeta_12')
   if (!o.IC) throw new Error('Verification key has no IC')
   return o
 }
@@ -47,9 +48,12 @@ export async function getDefaultRLNParams(treeDepth: number): Promise<IRLNParams
   if (!url) {
     return undefined
   }
-  const wasmFileURL = `${url}/circuit.wasm`
-  const finalZkeyURL = `${url}/final.zkey`
-  const verificationKeyURL = `${url}/verification_key.json`
+  const wasmFileURL = `${url}/RLN-20.wasm`
+  const finalZkeyURL = `${url}/contributions/rln-20_final.zkey`
+  console.log(url)
+  console.log(wasmFileURL)
+  console.log(finalZkeyURL)
+  const verificationKeyURL = `${url}/rln-20_vkey.json`
   const verificationKey = await downloadVerificationKey(verificationKeyURL)
   const [wasmFile, finalZkey] = await Promise.all([
     downloadBinary(wasmFileURL),
@@ -63,8 +67,8 @@ export async function getDefaultRLNParams(treeDepth: number): Promise<IRLNParams
 }
 
 export async function getDefaultWithdrawParams(): Promise<IWithdrawParams> {
-  const wasmFileURL = `${withdrawURL}/circuit.wasm`
-  const finalZkeyURL = `${withdrawURL}/final.zkey`
+  const wasmFileURL = `${withdrawURL}/RLN-Withdraw.wasm`
+  const finalZkeyURL = `${withdrawURL}/contributions/rln-withdraw_final.zkey`
   const [wasmFile, finalZkey] = await Promise.all([
     downloadBinary(wasmFileURL),
     downloadBinary(finalZkeyURL),
@@ -74,5 +78,3 @@ export async function getDefaultWithdrawParams(): Promise<IWithdrawParams> {
     finalZkey,
   }
 }
-
-
